e677272b4cfbcae5770eeff9727a8d42497480e2
akshithaganta <156398458+akshithaganta@users.noreply.github.com>
2024-01-12 10:29:24 +0530

    Create sas.py

---
+++ sas.py
@@ -0,0 +1,4 @@
+import streamlit as st
+st.header('sasuke')
+st.write('sasuke')
+st.image('https://w0.peakpx.com/wallpaper/28/814/HD-wallpaper-sasuke-uchiha-naruto-sasuke-uchiha.jpg',caption='sasuke')
